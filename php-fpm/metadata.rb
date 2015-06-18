efc129f562b8cc01b541efa5c38ded1f1dd5f2f6
Sergey Polyakov <polyakov_sergey@icloud.com>
2015-06-18 14:01:51 +0300

    no apt

--- php-fpm/metadata.rb
+++ php-fpm/metadata.rb
@@ -6,7 +6,7 @@ description      "Installs/Configures php-fpm"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.7.4"
 
-depends "apt"
+#depends "apt"
 depends "yum", ">= 3.0"
 
 %w{ debian ubuntu centos redhat fedora amazon }.each do |os|
