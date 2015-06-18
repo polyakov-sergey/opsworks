4ea8ea93f4da5fe4b5b51f38efd6f39069894118
Sergey Polyakov <polyakov_sergey@icloud.com>
2015-06-18 18:43:48 +0300

    quotes

--- php-fpm/recipes/configure.rb
+++ php-fpm/recipes/configure.rb
@@ -26,14 +26,14 @@ template node[:php-fpm][:conf_file] do
   notifies :restart, "service[php-fpm]"
 end
 
-unless node[:php-fpm][:pools].key?('www')
+unless node[:'php-fpm'][:pools].key?('www')
   php_fpm_pool 'www' do
     enable false
   end
 end
 
-if node[:php-fpm][:pools]
-  node[:php-fpm][:pools].each do |pool|
+if node[:'php-fpm'][:pools]
+  node[:'php-fpm'][:pools].each do |pool|
     if pool.is_a?(Array)
       pool_name = pool[0]
       pool = pool[1]
